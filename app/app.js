6f231e9eaa66876b2812c5fff80a24898ec6c5a9
Isaiah Fowler <isaiahjfowler7@gmail.com>
2024-03-27 19:01:53 -0500

    updated app.js

--- app/app.js
+++ app/app.js
@@ -1,14 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  setTimeout(() => {
-    console.log("Hello world")
-    res.json({message: "Hello world"})
-  }, 10000)
-})
+app.get("/", (req, res) => {
+  res.json({ message: "Hello world" });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
